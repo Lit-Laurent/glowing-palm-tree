0e7f450163be7699e2bfd7159a133043f8e2649b
Lit-Laurent <cooperjallen03@gmail.com>
2026-08-13 11:34:27 -0700

    remove some noobish scripts

--- .config/myScripts/zenSearch/zenSearch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-pidof alacritty -s > "$HOME/.config/myScripts/zenSearch/pid"
-
-read "?Search: " searchWhat
-URL="https://duckduckgo.com/?q=$searchWhat"
-zen-browser --blank-window $URL
-return
